f05c8de444e5f88ec34dbfe7b60daea933d76ec9
SomethingSomethingOrOther <abdoulgdia@gmail.com>
2023-08-06 16:46:07 -0400

    you can add a conditional as a return when using ternary ?

--- src/App.js
+++ src/App.js
@@ -27,15 +27,15 @@ function App() {
       <button onClick={handleReset}>Reset me</button>
         <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
         <div className="container">
-          <div style={{backgroundColor: bool && Number(inputValue)===1 ? "orange":""}}>1</div>
-          <div style={{backgroundColor: bool && Number(inputValue)===2 ? "orange":""}}>2</div>
-          <div style={{backgroundColor: bool && Number(inputValue)===3 ? "orange":""}}>3</div>
-          <div style={{backgroundColor: bool && Number(inputValue)===4 ? "orange":""}}>4</div>
-          <div style={{backgroundColor: bool && Number(inputValue)===5 ? "orange":""}}>5</div>
-          <div style={{backgroundColor: bool && Number(inputValue)===6 ? "orange":""}}>6</div>
-          <div style={{backgroundColor: bool && Number(inputValue)===7 ? "orange":""}}>7</div>
-          <div style={{backgroundColor: bool && Number(inputValue)===8 ? "orange":""}}>8</div>
-          <div style={{backgroundColor: bool && Number(inputValue)===9 ? "orange":""}}>9</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 1 ?  "orange":""):""}}>1</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 2 ? "orange":""):""}}>2</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 3 ? "orange":""):""}}>3</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 4 ? "orange":""):""}}>4</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 5 ? "orange":""):""}}>5</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 6 ? "orange":""):""}}>6</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 7 ? "orange":""):""}}>7</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 8 ? "orange":""):""}}>8</div>
+          <div className="numbers" style={{backgroundColor: bool ? (Number(inputValue) === 9 ? "orange":""):""}}>9</div>
         </div>
     </div>
   );
